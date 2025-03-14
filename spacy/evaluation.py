657dcb4bd0be4f7ad2d02ddffa7736120545057a
Kehina Manseri <manserikehina@gmail.com>
2025-03-14 12:42:41 +0100

    :knife: Deleting files

--- spacy/evaluation.py
+++
@@ -1,7 +0,0 @@
-import spacy
-
-nlp = spacy.load("./output/model-best")  
-doc = nlp("Elyë lantanë melmessë sonen; merilyë melmerya, ness' aranel. Arwen ëa óress' Elessarwa; náro vëaner ar canya ohtar; melilyes nan umiro melë le, Roccalas aranel turmawendë.")  
-
-for ent in doc.ents:
-    print(ent.text, ent.label_)
